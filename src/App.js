104a0dbae64a6189efa843508bd5f50d07178dc1
lalebdi <loai.alebdi@gmail.com>
2020-07-28 19:50:29 -0400

    router implemented and tested

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import Home from './components/Home';
 import Skincare from './components/Skincare';
 import Makeup from './components/Makeup';
 import NotFound from './components/NotFound';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1> App component </h1>
-      <Home />
-      <Skincare />
-      <Makeup />
-      <NotFound />
+      <Router>
+      <div>
+      <Switch>
+          <Route  exact path="/" component={Home}>
+          </Route>
+          <Route path="/skincare" component={Skincare}>
+          </Route>
+          <Route path="/makeup" component={Makeup}>
+          </Route>
+          <Route  component={NotFound}>
+          </Route>
+        </Switch>
+      </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
