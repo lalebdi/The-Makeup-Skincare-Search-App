c873a7574e27743fb7b85ab7b83c39a8b9c63584
lalebdi <loai.alebdi@gmail.com>
2020-07-28 19:44:21 -0400

    the home component is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 import Skincare from './components/Skincare';
 import Makeup from './components/Makeup';
-import NotFound from './components/NotFound'
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
       <h1> App component </h1>
+      <Home />
       <Skincare />
       <Makeup />
       <NotFound />
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                
+                <h4> Home Component</h4>
             </div>
         )
     }
