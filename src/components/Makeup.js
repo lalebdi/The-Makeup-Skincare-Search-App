ee76a9ac8a7c04cafe14540f26485374a65f546d
lalebdi <loai.alebdi@gmail.com>
2020-07-28 14:59:13 -0400

    added the components

---
+++ src/components/Makeup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <h2> I am the makeup Component</h2>
+            </div>
+        )
+    }
+}
---
+++ src/components/Skincare.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <h2> I am the skincare component</h2>
+            </div>
+        )
+    }
+}
