41e78d60e0d847b6b8171c3cdc1cfdd032f42939
lalebdi <loai.alebdi@gmail.com>
2020-07-28 16:00:50 -0400

    added some body to the component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>Page Not Found</h1>
+        <div className="NotFound-page">
+            <h2>Oh no, you've found my junior developer's homepage!!</h2>
+            <br />
+            <h4> Despite sleeping on the couch most of the day, my junior web developer still finds time to do some coding... </h4>
+            <br />
+            <button>Back to the home page</button>
         </div>
     )
 }
