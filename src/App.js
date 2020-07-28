758cc37ebfad70cc001ed0c4553bcde3650938d7
lalebdi <loai.alebdi@gmail.com>
2020-07-28 15:00:09 -0400

    added the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Skincare from './components/Skincare';
+import Makeup from './components/Makeup';
 
 function App() {
   return (
     <div className="App">
       <h1> App component </h1>
+      <Skincare />
+      <Makeup />
     </div>
   );
 }
