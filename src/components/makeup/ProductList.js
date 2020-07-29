674ae5a3d7de73ff7f09e36cb3984388e032acc4
lalebdi <loai.alebdi@gmail.com>
2020-07-29 15:45:32 -0400

    added the props here

--- src/components/makeup/ProductList.js
+++ src/components/makeup/ProductList.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Product from './Product';
 
-export default function ProductList() {
+export default function ProductList(props) {
+    let products = props.filteredProducts.map((product) => {
+        return <Product key={product.id} name={product.name} brand={product.brand} ingredient={product.ingredient_list} />
+    })
     return (
         <div>
-            
+            {products}
         </div>
     )
 }
