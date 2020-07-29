e4b7add449c26d9b4b10fd0bd08096441f85bd4c
lalebdi <loai.alebdi@gmail.com>
2020-07-28 20:44:41 -0400

    added the choice buttons and tested them

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,11 @@ export default class Home extends Component {
         return (
             <div className="home-page">
                 <h4> Home Component</h4>
+
+                <div className="buttons">
+                    <button><a href="/skincare" className="myButton">Skincare</a></button>
+                    <button><a href="/makeup" className="myButton">Makeup</a></button>
+                </div>
             </div>
         )
     }
