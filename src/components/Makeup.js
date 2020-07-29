8126fe548b3f06cc0ebd4770c9b07a51440303a3
lalebdi <loai.alebdi@gmail.com>
2020-07-29 09:17:11 -0400

    axios is done and is working

--- src/components/Makeup.js
+++ src/components/Makeup.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const BASE_URL = ''
+const BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json'
 
 export default class componentName extends Component {
     constructor(){
         super()
         this.state = {
-
+            info:[]
         };
     }
 
@@ -20,6 +20,16 @@ export default class componentName extends Component {
         return (
             <div className="makeup-component">
                 <h2> I am the makeup Component</h2>
+                {
+                    this.state.info.map(makeup => {
+                        console.log(makeup);
+                        return(
+                            <div>
+                                <h2> the name is: {makeup.name}</h2>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
--- src/components/Skincare.js
+++ src/components/Skincare.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const BASE_URL = ''
+const BASE_URL = 'https://skincare-api.herokuapp.com/product?q=rose&limit=25&page=1'
 
 export default class componentName extends Component {
     constructor(){
         super()
         this.state = {
-
+            info:[]
         };
     }
 
@@ -17,11 +17,22 @@ export default class componentName extends Component {
         .catch(err => console.error(err.message))
     }
 
-    
+
     render() {
         return (
             <div className="skincare-component">
+                
                 <h2> I am the skincare component</h2>
+                {
+                    this.state.info.map(cream =>
+                        {console.log(cream);
+                        return(
+                            <div>
+                                <h2> the name is : {cream.name}</h2>
+                            </div>
+                        )
+                        })
+                }
             </div>
         )
     }
