e37a82e96876698055e8d4e485c79a663abe2a18
lalebdi <loai.alebdi@gmail.com>
2020-07-29 15:47:17 -0400

    changed the attributes

--- src/components/makeup/Product.js
+++ src/components/makeup/Product.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Product() {
+export default function Product(props) {
     return (
         <div>
-            
+            <p> Name: {props.name} </p>
+            <p> Brand: {props.brand} </p>
+            <p> Description : {props.description}</p>
         </div>
     )
 }
--- src/components/makeup/ProductList.js
+++ src/components/makeup/ProductList.js
@@ -3,7 +3,7 @@ import Product from './Product';
 
 export default function ProductList(props) {
     let products = props.filteredProducts.map((product) => {
-        return <Product key={product.id} name={product.name} brand={product.brand} ingredient={product.ingredient_list} />
+        return <Product key={product.id} name={product.name} brand={product.brand} description={product.description} />
     })
     return (
         <div>
