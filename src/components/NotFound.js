1c7023e47e4b781eb302394ed72a4685f53c7db9
lalebdi <loai.alebdi@gmail.com>
2020-07-30 13:36:12 -0400

    done with the not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { NavLink }  from 'reactstrap';
+import Bella from '../assets/Images/junior developer.jpg';
 
 export default function NotFound() {
     return (
         <div className="NotFound-page">
+            <br />
+            <div className="not-found-text">
             <h2>Oh no, you've found my junior developer's homepage!!</h2>
             <br />
             <h4> Despite sleeping on the couch most of the day, my junior web developer still finds time to do some coding... </h4>
             <br />
-            <button>Back to the home page</button>
+            <button><NavLink href="/" >Back to the home page</NavLink></button>
+            </div>
+            <div className="bella" >
+                <img src={Bella} alt = "developer hard at work" className="bella-pic"/>
+            </div>
         </div>
     )
 }
