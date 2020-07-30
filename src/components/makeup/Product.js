d0da9a9585ad351d4284c08e7b6c27727626beaf
lalebdi <loai.alebdi@gmail.com>
2020-07-30 11:40:45 -0400

    added the cards to hold the products

--- src/components/makeup/Product.js
+++ src/components/makeup/Product.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {
+  Card, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
 
-export default function Product(props) {
-    return (
-        <div>
-            <p> Name: {props.name} </p>
-            <p> Brand: {props.brand} </p>
-            <p> Description : {props.description}</p>
-            <img src={props.image} /> 
-            <p> price: {props.price} </p>
-        </div>
-    )
-}
+const Example = (props) => {
+  return (
+    <div>
+      <Card className="card">
+        <CardBody>
+          <CardTitle>{props.brand}</CardTitle>
+          <CardSubtitle>{props.name}</CardSubtitle>
+        </CardBody>
+        <img width="25%" src={props.image} alt="Products" />
+        <CardBody>
+          <CardText>{props.description}</CardText>
+          <CardText> $ {props.price}</CardText>
+          <CardLink href={props.link}>Product Link</CardLink>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Example;
+
+
--- src/components/makeup/ProductList.js
+++ src/components/makeup/ProductList.js
@@ -3,7 +3,7 @@ import Product from './Product';
 
 export default function ProductList(props) {
     let products = props.filteredProducts1.map((product) => {
-        return <Product key={product.id} name={product.name} brand={product.brand} description={product.description} image={product.image_link} price={product.price}/>
+        return <Product key={product.id} name={product.name} brand={product.brand} description={product.description} image={product.image_link} price={product.price} link={product.product_link} />
     })
     return (
         <div>
